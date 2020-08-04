6cddf1d224f26d67735d81e315a74e8fa3c19b9e
myk245 <myk245@nyu.edu>
2020-08-03 22:41:47 -0400

    implement express for routing; sets ejs as the view engine; move CSS stylesheet to public folder

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-app.use(logger)
+// sets the view engine to EJS
+app.set('view engine', 'ejs');
+
+// directs Express to the public folder for stylesheets
+app.use(express.static("public"));
+
+// app.use(logger)
 
 // controller actions
 app.get('/', (req, res) => {
-   res.send('Home Page')
+   res.render('home')
 })
 
 app.get('/users', (req, res) => {
    res.send('Users Page')
 })
 
-function logger(req, res, next) {
-   console.log('Log')
-   next()
-}
+// function logger(req, res, next) {
+//    console.log('Log')
+//    next()
+// }
 
 app.listen(3000, () => console.log('Server Started'))
