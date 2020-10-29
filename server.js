fa8b146431a23fc2cce5f873a62aaa92fae6647c
myk245 <myk245@nyu.edu>
2020-10-29 00:43:44 -0400

    adds headers

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const cors = require('cors'); 
+
 const app = express();
+const port = process.env.SERVER_PORT || 3000;
 
-app.use(cors()); 
+app.use(cors());
 
-app.use(function (req, res, next) {
-   res.header("Access-Control-Allow-Origin", "*"); 
-   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-   next();
-});
+// app.use(function (req, res, next) {
+//    res.header("Access-Control-Allow-Origin", "*");
+//    res.header(
+//       "Access-Control-Allow-Headers",
+//       "Origin, X-Requested-With, Content-Type, Accept"
+//    );
+//    next();
+// });
 
 // sets the view engine to EJS
 app.set('view engine', 'ejs');
@@ -25,4 +30,4 @@ app.get('/log', (req, res) => {
    res.render('log')
 })
 
-app.listen(3000, () => console.log('Server Started'))
+app.listen(port, () => console.log(`Listening on port ${port}`));
