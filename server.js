57957d38a13ab1c167a99bad4a2662ee6d54841e
myk245 <myk245@nyu.edu>
2020-12-28 18:57:48 -0500

    require path

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+// sets the view engine to EJS
+app.set('view engine', 'ejs');
+
+// directs Express to the public folder for stylesheets
+app.use(express.static("public"));
+
+// app.use(logger)
+
+// controller actions
+app.get('/', (req, res) => {
+   res.render('home')
+})
+
+app.get('/log', (req, res) => {
+   res.render('log')
+})
+
+// function logger(req, res, next) {
+//    console.log('Log')
+//    next()
+// }
+
+app.listen(3000, () => console.log('Server Started'))
